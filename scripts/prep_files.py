ab66277fb8a0df50ebdac6cca8894891f5311f2d
iamdamosuzuki <humanherb@Humans-MacBook-Air.local>
2019-11-13 16:11:59 -0500

    made prep_files.py cross-platform compatible

--- scripts/prep_files.py
+++ scripts/prep_files.py
@@ -4,6 +4,8 @@
 import os, sys
 import argparse                     # used for parsing input arguments
 import subprocess
+from os.path import expanduser
+home = expanduser("~")
 
 def main():
 
@@ -19,8 +21,9 @@ def main():
         quit()
 
     in_path = args.i
-    my_dir = os.path.expanduser('~/Desktop')
-    out_path = my_dir + "/audio_in/"
+    my_dir = os.path.join(home, 'Desktop')
+    out_path = os.path.join(my_dir,"audio_in")
+    ffmpeg_path = os.path.join("ffmpeg")
 
     if not os.path.exists(out_path):
         os.mkdir(out_path)
@@ -29,12 +32,13 @@ def main():
 
         current_file_path = os.path.join(in_path, filename)
         noextension = os.path.splitext(filename)[0]
-
+        out_file = os.path.join(out_path, noextension + "_16kHz.wav")
+        print (out_file)
 
         if (filename.endswith(".mp3") or filename.endswith(".wav") or filename.endswith(".flac")):
-            cmd = "/usr/local/bin/ffmpeg -hide_banner -loglevel panic -i '" + current_file_path + "' -c:a pcm_s16le -ac 1 -ar 16000 -vn -dn -af 'pan=stereo|c0=c0+c1|c1=c0+c1' -y '" + out_path + noextension + "_16kHz.wav'"
+            cmd = ffmpeg_path + " -hide_banner -loglevel panic -i '" + current_file_path + "' -c:a pcm_s16le -ac 1 -ar 16000 -vn -dn -af 'pan=stereo|c0=c0+c1|c1=c0+c1' -y '" + out_file + "'"
         elif (filename.endswith(".mp4") or filename.endswith(".avi") or filename.endswith(".mov") or filename.endswith(".mpeg")): #or .avi, .mpeg, whatever.
-            cmd = "/usr/local/bin/ffmpeg -hide_banner -loglevel panic -i '" + current_file_path + "' -c:a pcm_s16le -ac 1 -ar 16000 -dn -af 'pan=stereo|c0=c0+c1|c1=c0+c1' -y '" + out_path + noextension + "_16kHz.wav'"
+            cmd = ffmpeg_path + " -hide_banner -loglevel panic -i '" + current_file_path + "' -c:a pcm_s16le -ac 1 -ar 16000 -dn -af 'pan=stereo|c0=c0+c1|c1=c0+c1' -y '" +  out_file + "'"
 
         #print(cmd)
         ffmpeg_out = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True).communicate()[0]
