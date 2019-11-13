127e915598eddcd4e6b3a6932a3564171fb95d22
Morgan Oscar Morel <morganmorel@bavc.org>
2019-11-13 15:10:00 -0500

    took out code that is redundant

--- setup.sh
+++ setup.sh
@@ -4,27 +4,29 @@ cd /audio_in/
 
 ## Now add media files to /audio_in/
 
-for file in *.{wav,mp3,mp4,WAV,MP3,MP4}; do
-if [ ${file: -4} == ".mp3" ]; then
-base=$(basename """$file""" .mp3);
-fi
-if [ ${file: -4} == ".MP3" ]; then
-base=$(basename """$file""" .MP3);
-fi
-if [ ${file: -4} == ".mp4" ]; then
-base=$(basename """$file""" .MP4);
-fi
-if [ ${file: -4} == ".MP4" ]; then
-base=$(basename """$file""" .MP4);
-fi
-if [ ${file: -4} == ".wav" ]; then
-base=$(basename """$file""" .wav);
-fi
-if [ ${file: -4} == ".WAV" ]; then
-base=$(basename """$file""" .WAV);
-fi
-ffmpeg -i """$file""" -ac 1 -ar 16000 """$base"""_16kHz.wav;
-done
+## The following block of code was removed because it was too processor intensive to do this work in docker. It's not done with prep_files.py locally`
+
+#for file in *.{wav,mp3,mp4,WAV,MP3,MP4}; do
+#if [ ${file: -4} == ".mp3" ]; then
+#base=$(basename """$file""" .mp3);
+#fi
+#if [ ${file: -4} == ".MP3" ]; then
+#base=$(basename """$file""" .MP3);
+#fi
+#if [ ${file: -4} == ".mp4" ]; then
+#base=$(basename """$file""" .MP4);
+#fi
+#if [ ${file: -4} == ".MP4" ]; then
+#base=$(basename """$file""" .MP4);
+#fi
+#if [ ${file: -4} == ".wav" ]; then
+#base=$(basename """$file""" .wav);
+#fi
+#if [ ${file: -4} == ".WAV" ]; then
+#base=$(basename """$file""" .WAV);
+#fi
+#ffmpeg -i """$file""" -ac 1 -ar 16000 """$base"""_16kHz.wav;
+#done
 
 mkdir /audio_in_16khz/
 mv *_16kHz.wav /audio_in_16khz/
